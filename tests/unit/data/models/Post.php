52e75cf35f4dfa2ee55c367b68dee5b90a62140e
Antonio Ramirez <amigo.cobos@gmail.com>
2015-03-06 16:46:58 +0100

    removed wrong ignored file location

---
+++ tests/unit/data/models/Post.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace tests\data\models;
+
+use yii\db\ActiveRecord;
+
+class Post extends ActiveRecord
+{
+    public $message;
+    public static $db;
+
+    public static function getDb()
+    {
+        return self::$db;
+    }
+}
